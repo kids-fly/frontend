1e765471d0908c97fea6bed95203bf6704547255
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-29 19:28:19 -0400

    hero image complete have to style text though

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+import img from "../img/plane.jpg";
 import { makeStyles } from "@material-ui/core/styles";
 
+const Hero = styled.div`
+  margin-top: 10px;
+  height: 500px;
+  width: 100%;
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
+
 const useStyles = makeStyles({
-  paper: {
-    marginTop: "30px",
-    border: "none"
+  text: {
+    fontSize: "20px",
+    color: "white",
+    paddingTop: "300px",
+    paddingLeft: "10px"
   }
 });
 
@@ -13,17 +28,12 @@ const HeroImage = () => {
   const classes = useStyles();
   return (
     <div>
-      <main>
-        <Paper className={classes.paper}>
-          {
-            <img
-              src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/5EaiPi6/delta-airlines-plane-takes-off-airport-runway_b9fixl78l_thumbnail-full13.png"
-              alt="background"
-              style={{ width: "100%", height: "500px" }}
-            />
-          }
-        </Paper>
-      </main>
+      <Hero>
+        <Typography className={classes.text}>
+          Our mission is to give parents a pleasant flight experience
+        </Typography>
+        <Button>Get Started</Button>
+      </Hero>
     </div>
   );
 };
