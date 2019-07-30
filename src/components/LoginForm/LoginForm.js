cdf9007263d82901c87a9e3aa7f03a9b727a8b4b
Sunil Karki <karkisunil1200@gmail.com>
2019-07-30 12:21:15 +0545

    Login page and sign up page

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import './LoginForm.css';
+import {Link} from 'react-router-dom';
+
+function LoginForm() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='container login-form'>
+      <h2 className='dipslay-4 text-center py-5'>Enter your Login Information</h2>
+      <form onSubmit={handleSubmit}>
+        <div class='form-group'>
+          <label>Name</label>
+          <input
+            type='text'
+            className='form-control'
+            value={formState.name}
+            onChange={handleChange}
+            placeholder='Enter name'
+            name='name'
+            autoComplete='off'
+          />
+        </div>
+        <div className='form-group'>
+          <label>Email address</label>
+          <input
+            type='email'
+            className='form-control'
+            value={formState.email}
+            onChange={handleChange}
+            placeholder='Enter email'
+            name='email'
+            autoComplete='off'
+          />
+        </div>
+        <div className='form-group'>
+          <label>Password</label>
+          <input
+            type='password'
+            className='form-control'
+            value={formState.password}
+            onChange={handleChange}
+            placeholder='Password'
+            name='password'
+            autoComplete='off'
+          />
+        </div>
+
+        <button type='submit' className='btn btn-success btn-block'>
+          Login
+        </button>
+        <small>
+          Need account{' '}
+          <span className='text-danger'>
+            <Link to='/signup'>Sign up</Link>
+          </span>
+        </small>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
