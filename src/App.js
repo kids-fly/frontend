1f425ff5e559347bfe80db7b2c7d3d2aa1b6f0f8
Chike Chris <cchike2017@gmail.com>
2019-07-31 03:02:15 -0400

    building out the bookFlight function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Form</h2>
+        <h2>kidsFly</h2>
       </header>
       <RequestForm />
     </div>
--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -1,6 +1,93 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+const FormStyle = styled.div`
+  body {
+    width: 100%;
+    height: 100%;
+
+    margin-top: 10%;
+    transform: translateY(-20%);
+
+    position: absolute;
+
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+  }
+
+  .form-container {
+    margin: auto;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    background-size: cover;
+
+    justify-content: center;
+  }
+  form {
+    display: flex;
+    max-width: 50%;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+
+    flex-direction: column;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    padding: 30px;
+    border-radius: 5px;
+  }
+
+  h2 {
+    font-family: "Serif ", "Georgia ";
+    text-shadow: 0 0 1px #ff0000, 0 0 2px #c5ecfd;
+  }
+  input {
+    display: flex;
+    flex-direction: column;
+    margin-right: 4%;
+    font-family: "Serif ", "Georgia ";
+    margin: 5px 0;
+    background: transparent;
+    border: 0px;
+    border-bottom: 2px solid #c5ecfd;
+    padding: 10px;
+    color: "white";
+    width: 100%;
+  }
+  ${"" /* input[type="radio"] {
+    opacity: 0;
+    width: 0;
+    margin: 0;
+  } */}
+
+  label[for="father"],
+  label[for="mother"] {
+    margin: 0;
+    display: flex;
+    flex-direction: row;
+    padding: 0px;
+    font-size: 10px;
+  }
+  button {
+    background: rgb(176, 192, 150);
+    text-align: center;
+    padding: 5px;
+    border-radius: 30px;
+    color: #0000ff;
+    cursor: pointer;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+  }
+`;
+
 function RequestForm() {
   const [users, setUsers] = useState({
     name: "",
@@ -29,10 +116,18 @@ function RequestForm() {
   };
 
   return (
-    <div>
+    <FormStyle>
       <div className="form-container">
         <form onSubmit={handleSubmit}>
           <h2 className="login_title">Schedule Assistance Form</h2>
+
+          <p>You are a....</p>
+          <input type="radio" id="father" />
+          <label for="father">Father</label>
+
+          <input type="radio" id="mother" />
+          <label for="mother">Mother</label>
+          <br />
           <input
             type="text"
             name="name"
@@ -57,7 +152,7 @@ function RequestForm() {
           <input
             type="text"
             name="flight"
-            placeholder="Enter your Flight number"
+            placeholder="Enter your Flight Number"
             value={users.flight}
             onChange={handleChange}
           />
@@ -84,7 +179,7 @@ function RequestForm() {
           </button>
         </form>
       </div>
-    </div>
+    </FormStyle>
   );
 }
 
