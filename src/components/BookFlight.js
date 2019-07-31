661ff734c0ec1808b43e98a897fa4f15e8d0eb93
Chike Chris <cchike2017@gmail.com>
2019-07-31 17:34:47 -0400

    updated the form

--- src/components/BookFlight.js
+++ src/components/BookFlight.js
@@ -1,173 +1,173 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+// import React, { useState } from "react";
+// import styled from "styled-components";
+// import { Link } from "react-router-dom";
 
-const BookStyle = styled.div`
-  body {
-    width: 100%;
-    height: 100%;
+// const BookStyle = styled.div`
+//   body {
+//     width: 100%;
+//     height: 100%;
 
-    margin-top: 10%;
-    transform: translateY(-20%);
+//     margin-top: 10%;
+//     transform: translateY(-20%);
 
-    position: absolute;
+//     position: absolute;
 
-    display: flex;
-    align-items: center;
-    align-content: center;
-    justify-content: center;
-  }
+//     display: flex;
+//     align-items: center;
+//     align-content: center;
+//     justify-content: center;
+//   }
 
-  .form-container {
-    margin: auto;
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    align-items: center;
-    background-size: cover;
+//   .form-container {
+//     margin: auto;
+//     display: flex;
+//     align-content: center;
+//     justify-content: center;
+//     align-items: center;
+//     background-size: cover;
 
-    justify-content: center;
-  }
-  form {
-    display: flex;
-    max-width: 50%;
-    width: 100%;
-    height: 100%;
-    margin: 0 auto;
-    align-content: center;
-    justify-content: center;
-    align-items: center;
+//     justify-content: center;
+//   }
+//   form {
+//     display: flex;
+//     max-width: 50%;
+//     width: 100%;
+//     height: 100%;
+//     margin: 0 auto;
+//     align-content: center;
+//     justify-content: center;
+//     align-items: center;
 
-    flex-direction: column;
-    align-items: center;
-    align-content: center;
-    justify-content: center;
-    padding: 30px;
-    border-radius: 5px;
-  }
+//     flex-direction: column;
+//     align-items: center;
+//     align-content: center;
+//     justify-content: center;
+//     padding: 30px;
+//     border-radius: 5px;
+//   }
 
-  h2 {
-    font-family: "Serif ", "Georgia ";
-    text-shadow: 0 0 1px #ff0000, 0 0 2px #c5ecfd;
-  }
-  input {
-    display: flex;
-    flex-direction: column;
-    margin-right: 4%;
-    font-family: "Serif ", "Georgia ";
-    margin: 5px 0;
-    background: transparent;
-    border: 0px;
-    border-bottom: 2px solid #c5ecfd;
-    padding: 10px;
-    color: "white";
-    width: 100%;
-  }
+//   h2 {
+//     font-family: "Serif ", "Georgia ";
+//     text-shadow: 0 0 1px #ff0000, 0 0 2px #c5ecfd;
+//   }
+//   input {
+//     display: flex;
+//     flex-direction: column;
+//     margin-right: 4%;
+//     font-family: "Serif ", "Georgia ";
+//     margin: 5px 0;
+//     background: transparent;
+//     border: 0px;
+//     border-bottom: 2px solid #c5ecfd;
+//     padding: 10px;
+//     color: "white";
+//     width: 100%;
+//   }
 
-  button {
-    background: rgb(176, 192, 150);
-    text-align: center;
-    padding: 5px;
-    border-radius: 30px;
-    color: #0000ff;
-    cursor: pointer;
-    text-transform: uppercase;
-    margin-bottom: 10px;
-  }
-`;
+//   button {
+//     background: rgb(176, 192, 150);
+//     text-align: center;
+//     padding: 5px;
+//     border-radius: 30px;
+//     color: #0000ff;
+//     cursor: pointer;
+//     text-transform: uppercase;
+//     margin-bottom: 10px;
+//   }
+// `;
 
-function BookFlight() {
-  const [flight, setFlight] = useState({
-    cityFrom: "",
-    cityTo: "",
-    depart: "",
-    timeOfdayD: "",
-    return: "",
-    timeOfdayR: ""
-  });
+// function BookFlight() {
+//   const [flight, setFlight] = useState({
+//     cityFrom: "",
+//     cityTo: "",
+//     depart: "",
+//     timeOfdayD: "",
+//     return: "",
+//     timeOfdayR: ""
+//   });
 
-  const handleChange = e => {
-    setFlight({ ...flight, [e.target.name]: e.target.value });
-  };
+//   const handleChange = e => {
+//     setFlight({ ...flight, [e.target.name]: e.target.value });
+//   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
+//   const handleSubmit = e => {
+//     e.preventDefault();
 
-    setFlight({
-      cityFrom: "",
-      cityTo: "",
-      depart: "",
-      timeOfdayD: "",
-      return: "",
-      timeOfdayR: ""
-    });
-  };
+//     setFlight({
+//       cityFrom: "",
+//       cityTo: "",
+//       depart: "",
+//       timeOfdayD: "",
+//       return: "",
+//       timeOfdayR: ""
+//     });
+//   };
 
-  return (
-    <BookStyle>
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <h2 className="login_title">Book Flight</h2>
-          <label>From:</label>
-          <input
-            type="text"
-            name="cityFrom"
-            placeholder="city"
-            value={flight.cityFrom}
-            onChange={handleChange}
-          />
-          <label>To:</label>
-          <input
-            type="text"
-            name="cityTo"
-            placeholder="city"
-            value={flight.cityTo}
-            onChange={handleChange}
-          />
-          <label>Depart:</label>
-          <input
-            type="text"
-            name="depart"
-            placeholder="select date"
-            value={flight.date}
-            onChange={handleChange}
-          />
-          <label>Time Of Day:</label>
-          <input
-            type="text"
-            name=" timeOfdayD"
-            placeholder="choose time"
-            value={flight.timeOfdayD}
-            onChange={handleChange}
-          />
-          <label>Return:</label>
-          <input
-            type="text"
-            name="return"
-            placeholder="choose date"
-            value={flight.return}
-            onChange={handleChange}
-          />
-          <label>Time Of Day:</label>
-          <input
-            type="text"
-            name="timeOfdayR"
-            placeholder="choose time"
-            value={flight.timeOfdayR}
-            onChange={handleChange}
-          />
-          <button
-            onSubmit={handleSubmit}
-            onClick={handleSubmit}
-            className="login_button"
-          >
-            Search Flight
-          </button>
-          <Link to="/">GO Back</Link>
-        </form>
-      </div>
-    </BookStyle>
-  );
-}
+//   return (
+//     <BookStyle>
+//       <div className="form-container">
+//         <form onSubmit={handleSubmit}>
+//           <h2 className="login_title">Book Flight</h2>
+//           <label>From:</label>
+//           <input
+//             type="text"
+//             name="cityFrom"
+//             placeholder="city"
+//             value={flight.cityFrom}
+//             onChange={handleChange}
+//           />
+//           <label>To:</label>
+//           <input
+//             type="text"
+//             name="cityTo"
+//             placeholder="city"
+//             value={flight.cityTo}
+//             onChange={handleChange}
+//           />
+//           <label>Depart:</label>
+//           <input
+//             type="text"
+//             name="depart"
+//             placeholder="select date"
+//             value={flight.date}
+//             onChange={handleChange}
+//           />
+//           <label>Time Of Day:</label>
+//           <input
+//             type="text"
+//             name=" timeOfdayD"
+//             placeholder="choose time"
+//             value={flight.timeOfdayD}
+//             onChange={handleChange}
+//           />
+//           <label>Return:</label>
+//           <input
+//             type="text"
+//             name="return"
+//             placeholder="choose date"
+//             value={flight.return}
+//             onChange={handleChange}
+//           />
+//           <label>Time Of Day:</label>
+//           <input
+//             type="text"
+//             name="timeOfdayR"
+//             placeholder="choose time"
+//             value={flight.timeOfdayR}
+//             onChange={handleChange}
+//           />
+//           <button
+//             onSubmit={handleSubmit}
+//             onClick={handleSubmit}
+//             className="login_button"
+//           >
+//             Search Flight
+//           </button>
+//           <Link to="/">GO Back</Link>
+//         </form>
+//       </div>
+//     </BookStyle>
+//   );
+// }
 
-export default BookFlight;
+// export default BookFlight;
--- src/components/PlanTravel.js
+++ src/components/PlanTravel.js
@@ -1,43 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import styled from "styled-components";
 
-const Button = styled.button`
-  background: rgb(176, 192, 150);
-  text-align: center;
-  padding: 5px;
-  border-radius: 30px;
-  color: #0000ff;
-  cursor: pointer;
-  text-transform: uppercase;
-  margin-bottom: 10px;
-`;
+// const Button = styled.button`
+//   background: rgb(176, 192, 150);
+//   text-align: center;
+//   padding: 5px;
+//   border-radius: 30px;
+//   color: #0000ff;
+//   cursor: pointer;
+//   text-transform: uppercase;
+//   margin-bottom: 10px;
+// `;
 
-const H1 = styled.h1`
-  font-family: "Serif ", "Georgia ";
-  text-shadow: 0 0 1px #ff0000, 0 0 2px #c5ecfd;
-`;
+// const H1 = styled.h1`
+//   font-family: "Serif ", "Georgia ";
+//   text-shadow: 0 0 1px #ff0000, 0 0 2px #c5ecfd;
+// `;
 
-function PlanTravel() {
-  return (
-    <div>
-      <H1>Plan Your Travel</H1>
-      Schedule Your Flight:
-      <Link to="/bookflight">
-        <Button>Click</Button>
-      </Link>
-      <br />
-      Traveliving With Kids:
-      <Link to="/requestform">
-        <Button>Click</Button>
-      </Link>
-      <br />
-      Unaccompanied Kids:
-      <Link to="/minors">
-        <Button>Click</Button>
-      </Link>
-    </div>
-  );
-}
+// function PlanTravel() {
+//   return (
+//     <div>
+//       <H1>Plan Your Travel</H1>
+//       Schedule Your Flight:
+//       <Link to="/bookflight">
+//         <Button>Click</Button>
+//       </Link>
+//       <br />
+//       Traveliving With Kids:
+//       <Link to="/requestform">
+//         <Button>Click</Button>
+//       </Link>
+//       <br />
+//       Unaccompanied Kids:
+//       <Link to="/minors">
+//         <Button>Click</Button>
+//       </Link>
+//     </div>
+//   );
+// }
 
-export default PlanTravel;
+// export default PlanTravel;
