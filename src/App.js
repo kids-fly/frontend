4e7e903514f12b52af67e9d4418039499baef50e
Chike Chris <cchike2017@gmail.com>
2019-08-01 10:46:38 -0400

    modified request form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React, { useState } from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import { Switch, Route } from "react-router-dom";
-// import RequestForm from "./components/RequestForm";
-// import BookFlight from "./components/BookFlight";
-// import PlanTravel from "./components/PlanTravel";
-import Book from "./components/Book";
+
+import RequestForm from "./components/RequestForm";
 
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState({});
+
+  const [clients, setClients] = useState([]);
+
+  const [clientEdit, setClientEdit] = useState({});
+
+  const handleMemberToEdit = user => {
+    setClientEdit({ ...user });
+  };
+
+  const handleChange = event => {
+    setUsers({ ...users, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // saveArray();
+    editMember();
+    setUsers({
+      name: "",
+      selectFlight: "",
+      numberOfKids: "",
+      selectDepartureAdmin: "",
+      selectArrivalAdmin: ""
+    });
+  };
+
+  function editMember() {
+    setClients([...clients.filter(user => user.name !== users.name), users]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>kidsFly</h2>
-      </header>
-      {/* <Router>
-        <Switch>
-          <Route exact path="/" component={PlanTravel} />
-          <Route path="/requestform" component={RequestForm} />
-          <Route path="/bookflight" component={BookFlight} />
-          <Route path="/minors" component={Minors} />
-        </Switch>
-      </Router> */}
-      <Book />
+      <RequestForm
+        users={users}
+        setUsers={setUsers}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        clientEdit={clientEdit}
+      />
+
+      <div className="team-container">
+        {clients.map((user, index) => (
+          <div className="user-container" key={index}>
+            <h2>Confirm your Booking</h2>
+            <h3>Name: {user.name}</h3>
+            <h4>Flight Information: {user.selectFlight}</h4>
+            <h4>Number Of Kids: {user.numberOfKids}</h4>
+            <h4>Depature Admin: {user.selectDepartureAdmin}</h4>
+            <h4>Arrival Admin: {user.selectArrivalAdmin}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const FormStyle = styled.div`
+const MinorStyle = styled.div`
   body {
     width: 100%;
     height: 100%;
@@ -48,9 +48,31 @@ const FormStyle = styled.div`
 
   h2 {
     font-family: "Serif ", "Georgia ";
-    text-shadow: 0 0 1px #ff0000, 0 0 2px #c5ecfd;
+    text-shadow: 0 0 1px #1ab31a; 0 0 2px #1ab31a;
   }
   input {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin-right: 4%;
+    font-family: "Serif ", "Georgia ";
+    margin: 5px 0;
+    background: transparent;
+    border: 0px;
+    border-bottom: 2px solid #1ab31a;
+    padding: 10px;
+    color: "white";
+    width: 80%;
+  }
+
+  label{
+    font-style: italic;
+    background: lightgray;
+    text-shadow: 0 0 1px #1ab31a; 0 0 2px #1ab31a;
+    margin-top: 10px;
+    padding: 5px;
+  }
+  select {
     display: flex;
     flex-direction: column;
     margin-right: 4%;
@@ -58,27 +80,15 @@ const FormStyle = styled.div`
     margin: 5px 0;
     background: transparent;
     border: 0px;
-    border-bottom: 2px solid #c5ecfd;
+    border-bottom: 2px solid #1ab31a;
     padding: 10px;
     color: "white";
-    width: 100%;
+    width: 85%;
+    margin-bottom: 17px;
   }
-  ${"" /* input[type="radio"] {
-    opacity: 0;
-    width: 0;
-    margin: 0;
-  } */}
 
-  label[for="father"],
-  label[for="mother"] {
-    margin: 0;
-    display: flex;
-    flex-direction: row;
-    padding: 0px;
-    font-size: 10px;
-  }
   button {
-    background: rgb(176, 192, 150);
+    background: #1ab31a;
     text-align: center;
     padding: 5px;
     border-radius: 30px;
@@ -89,38 +99,14 @@ const FormStyle = styled.div`
   }
 `;
 
-function RequestForm() {
-  const [users, setUsers] = useState({
-    name: "",
-    email: "",
-    phoneNumber: "",
-    flight: "",
-    arrivalTime: "",
-    numberOfKids: ""
-  });
-
-  const handleChange = e => {
-    setUsers({ ...users, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-
-    setUsers({
-      name: "",
-      email: "",
-      phoneNumber: "",
-      flight: "",
-      arrivalTime: "",
-      numberOfKids: ""
-    });
-  };
-
+function RequestForm({ users, handleChange, handleSubmit }) {
   return (
-    <FormStyle>
+    <MinorStyle>
       <div className="form-container">
         <form onSubmit={handleSubmit}>
-          <h2 className="login_title">Assistance Request</h2>
+          <h2 className="login_title">Request For Assistance</h2>
+
+          <label>Name</label>
           <input
             type="text"
             name="name"
@@ -128,52 +114,51 @@ function RequestForm() {
             value={users.name}
             onChange={handleChange}
           />
+          <label>Number Of Kids</label>
           <input
             type="text"
-            name="email"
-            placeholder="Enter your email"
-            value={users.email}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="phoneNumber"
-            placeholder="Enter your Phone Number"
-            value={users.phoneNumber}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="flight"
-            placeholder="Enter your Flight Number"
-            value={users.flight}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="arrivalTime"
-            placeholder="Enter your Arrival Time"
-            value={users.arrivalTime}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
             name="numberOfKids"
             placeholder="Enter Number Of Kids"
             value={users.numberOfKids}
             onChange={handleChange}
           />
-          <button
-            onSubmit={handleSubmit}
-            onClick={handleSubmit}
-            className="login_button"
-          >
-            Submit Form
-          </button>
-          <Link to="/">GO Back</Link>
+          <label>Select Flight Information</label>
+          <select name="selectFlight" onChange={handleChange}>
+            <option value="" />
+            <option value="ATL to LKM 5:25 am Flight # 213">
+              ATL to LKM 5:am Flight # 213
+            </option>
+            <option value="ATL to CBV 10:30 am Flight # 473">
+              ATL to CBV 10:am Flight # 473
+            </option>
+            <option value="LOS to UIJ 5:00 am Flight # 740">
+              LOS to UIJ 5:am Flight # 740
+            </option>
+            <option value="BAL to CJH 4:20 pm Flight # 002">
+              BAL to CJH 4:20pm Flight # 002
+            </option>
+            <option value="NYC to Nepal 11:27 am Flight # 652">
+              NYC to Nepal 5:am Flight # 652
+            </option>
+          </select>
+          <label>Do you need a Departure Admin?</label>
+          <select name="selectDepartureAdmin" onChange={handleChange}>
+            <option value="" />
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+
+          <label>Do you need an Arrival Admin?</label>
+          <select name="selectArrivalAdmin" onChange={handleChange}>
+            <option value="" />
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+
+          <button onClick={handleSubmit}>Submit</button>
         </form>
       </div>
-    </FormStyle>
+    </MinorStyle>
   );
 }
 
